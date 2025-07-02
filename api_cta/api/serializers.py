747d192efd49593ad2c67e35ce63628db2fc8a2c
Carlos98770 <eduardoabc.eduardo9@gmail.com>
2025-07-02 16:18:53 -0300

    Ajustando o serializer da ubs e endereco

--- api_cta/api/serializers.py
+++ api_cta/api/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Medico, Ubs
+from .models import Medico, Ubs, Endereco
+
+class EnderecoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endereco
+        fields = '__all__'
 
 class MedicoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +12,9 @@ class MedicoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UbsSerializer(serializers.ModelSerializer):
+    endereco = EnderecoSerializer()
+    medicos = MedicoSerializer(many=True)
+
     class Meta:
         model = Ubs
         fields = '__all__'
--- api_cta/api/urls.py
+++ api_cta/api/urls.py
@@ -4,6 +4,6 @@ from .views import MedicoList ,UbsList, RegistrarPresenca
 urlpatterns = [
     path('medicos/', MedicoList.as_view(), name='medico-list'),
     path('ubs/', UbsList.as_view(), name='ubs-list'),
-    path('registrar-presenca', RegistrarPresenca.as_view(), name='presenca'),
+    path('presenca/', RegistrarPresenca.as_view(), name='presenca'),
 
 ]
